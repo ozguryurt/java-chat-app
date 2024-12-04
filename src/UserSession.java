30610223f626125423b3dce370c9797c58ec79f3
Özgür Yurt <ozgurryurtt@gmail.com>
2024-12-04 21:00:05 +0300

    UserSession sınıfı ile giriş yapan kullanıcı bilgilerinin depolanması

---
+++ src/UserSession.java
@@ -0,0 +1,39 @@
+public class UserSession {
+    private static UserSession instance;
+    private String username;
+    private int userId;
+    private int roomId;
+
+    private UserSession() {}
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+}
