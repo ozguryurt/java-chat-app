62c208763c2c35a67f30f2605d8281bd5f473a7c
Özgür Yurt <ozgurryurtt@gmail.com>
2024-12-06 13:56:47 +0300

    Yorum satırları eklendi

--- src/HashingUtil.java
+++ src/HashingUtil.java
@@ -4,12 +4,14 @@ import java.security.NoSuchAlgorithmException;
 public class HashingUtil {
     public static String hashPassword(String password) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(password.getBytes());
-            StringBuilder hexString = new StringBuilder();
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); // SHA-256 algoritmasını temsil eden bir MessageDigest nesnesi oluşturur.
+            byte[] hash = md.digest(password.getBytes()); // String tipindeki şifreyi byte dizisine çevirir.
+            // md.digest() ise bu byte dizisini hash'leyerek SHA-256 algoritmasına göre bir byte dizisi oluşturur.
+            StringBuilder hexString = new StringBuilder(); // SHA-256 algoritmasından gelen byte dizisi, okunamaz, dönen her bir byte, 16'lık taban (hexadecimal) formatına çevrilir:
             for (byte b : hash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) hexString.append('0');
+                // bit seviyesinde bir AND işlemi yapılır, 
+                String hex = Integer.toHexString(0xff & b); // 0xff & b: Byte'in negatif olmamasını sağlamak için maskelenir. (integerı hexadecimal stringe çevir)
+                if (hex.length() == 1) hexString.append('0'); // Integer.toHexString(...): Sayıyı hexadecimal string'e dönüştürür.
                 hexString.append(hex);
             }
             return hexString.toString();
